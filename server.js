65cd7d1829eba644b18e410fa7beade33d4c993c
Alisamar Husain <zrthxn@gmail.com>
2019-06-03 02:40:04 +0530

    Port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = require('./config.json').PORT || 5000;
 const server = express()
+
+const PORT = require('./config.json').PORT || 5000;
+const config = require('./config.json')
 
 server.use('/static', path.join(__dirname, 'static'))
 
