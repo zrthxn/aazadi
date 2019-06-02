22748a07c9a320709c49f2b683cebf3213124c9f
Alisamar Husain <zrthxn@gmail.com>
2019-06-03 02:36:45 +0530

    Port

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000;
+const PORT = require('./config.json').PORT || 5000;
 const server = express()
 
 server.use('/static', path.join(__dirname, 'static'))
 
-server.listen(PORT, ()=>{
+server.listen(PORT, ()=> console.log('Aazadi :: Started ::', 'PORT', config.PORT))
 
+server.get('*', (req,res)=>{
+  res.sendFile( path.resolve(__dirname, 'docs', 'index.html') )
 })
